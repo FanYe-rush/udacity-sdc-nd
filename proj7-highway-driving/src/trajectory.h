13b2ace2a8c49425cbcd07a6003947f5d44be8d8
FanYe-rush <yefan1994@gmail.com>
2021-02-11 19:44:56 -0800

    fix trail

--- proj7-highway-driving/src/trajectory.h
+++ proj7-highway-driving/src/trajectory.h
@@ -22,22 +22,62 @@ vector<double> generateLongiSpeedKeepingPathPoly(Ego ego, const vector<Car> &tra
 
 vector<vector<double>> generateTrajectory(Ego &ego, State next, vector<Car> traffic, const Mapdata &map,
                                const vector<double> &prev_x_vals, const vector<double> &prev_y_vals,
-                              double prev_s, double prev_d) {
+                                           double prev_s, double prev_d) {
+  double ref_x;
+  double prev_x;
+  double ref_y;
+  double prev_y;
+  double dx;
+  double dy;
+  double ref_yaw;
+  double vx;
+  double vy;
+  double ref_v;
   
-  vector<vector<double>> extension;
+  if (prev_x_vals.size() < 2) {
+    ref_x = ego.x;
+    prev_x = ego.x - cos(deg2rad(ego.yaw));
+    ref_y = ego.y;
+    prev_y = ego.y - sin(deg2rad(ego.yaw));
+    
+    ref_yaw = ego.yaw;
+    ref_v = ego.v;
+  } else {
+    ref_x = prev_x_vals[prev_x_vals.size()-1];
+    prev_x = prev_x_vals[prev_x_vals.size()-2];
+    ref_y = prev_y_vals[prev_y_vals.size()-1];
+    prev_y = prev_y_vals[prev_y_vals.size()-2];
+    
+    dx = ref_x - prev_x;
+    dy = ref_y - prev_y;
+    
+    ref_yaw = rad2deg(atan2(dy, dx));
+    
+    vx = dx / 0.02;
+    vy = dy / 0.02;
+    
+    ref_v = sqrt(vx*vx+vy*vy);
+  }
+  
+
+  vector<vector<double>> trail;
+  trail.push_back({prev_x, prev_y});
+  trail.push_back({ref_x, ref_y});
+
   Ego extended_ego;
-  
-  extension = extendPreviousPath(prev_x_vals, prev_y_vals, 3);
-  
-  extended_ego.yaw = getCurrentAngle(prev_x_vals, prev_y_vals);
-  extended_ego.v = getCurrentSpeed(prev_x_vals, prev_y_vals);
-  extended_ego.x = extension[extension.size()-1][0];
-  extended_ego.y = extension[extension.size()-1][1];
-  vector<double> sd = getFrenet(extended_ego.x, extended_ego.y, deg2rad(extended_ego.yaw), map.x, map.y);
-  extended_ego.s = sd[0];
-  extended_ego.d = sd[1];
-  
-  return generateKeepLaneTrajectory(extended_ego, traffic, map, extension);
+  extended_ego.yaw = ref_yaw;
+  extended_ego.v = ref_v;
+  extended_ego.x = ref_x;
+  extended_ego.y = ref_y;
+  if (prev_x_vals.size() > 0) {
+    extended_ego.s = prev_s;
+    extended_ego.d = prev_d;
+  } else {
+    extended_ego.s = ego.s;
+    extended_ego.d = ego.d;
+  }
+
+  return generateKeepLaneTrajectory(extended_ego, traffic, map, trail);
 //
 //  if (next == KL) {
 //    return generateKeepLaneTrajectory(extended_ego, traffic, map, extension);
